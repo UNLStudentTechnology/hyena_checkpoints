8c77764a1205156366afa824238614adbd09a99e
Hayden Gascoigne <haydengascoigne@gmail.com>
2014-12-09 16:33:39 -0600

    Commit for 12/9

--- web.js
+++ web.js
@@ -5,4 +5,7 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.all("/*", function(req, res, next) {
+    res.sendfile("index.html", { root: __dirname + "/dist" });
+});
 app.listen(process.env.PORT || 5000);
